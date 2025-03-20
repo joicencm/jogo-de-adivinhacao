13c36584f3fd25672f41022e7dfa2432b15c1e55
joicencm <joicencm@gmail.com>
2025-03-20 20:07:54 -0300

    Implementa Primeira Versão

--- JogoDeAdivinhacaoConsoleApp1/Program.cs
+++ JogoDeAdivinhacaoConsoleApp1/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace JogoDeAdivinhacaoConsoleApp1
+﻿
+namespace JogoDeAdivinhacaoConsoleApp1
 {
     internal class Program
     {
+        //Versão1: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("--------------------------------------");
+
+            //Logica do jogo
+            Console.Write("Digite um número para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o nemero: " + numeroDigitado);
+
+            Console.ReadLine();
         }
     }
 }
