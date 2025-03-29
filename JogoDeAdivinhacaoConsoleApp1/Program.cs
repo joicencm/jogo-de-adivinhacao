dc1b16285646d652cb24ec1a3f2808391c0e5aab
joicencm <joicencm@gmail.com>
2025-03-28 21:34:53 -0300

    Refatoração - Programação Estruturada

--- JogoDeAdivinhacaoConsoleApp1/Program.cs
+++ JogoDeAdivinhacaoConsoleApp1/Program.cs
@@ -6,35 +6,15 @@ namespace JogoDeAdivinhacaoConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+
             string[] numerosDigitados = new string[100];
             int pontos = 500;
 
             while (true)
             {
-                Console.WriteLine("---------------------------------------------------------------------------------------------");
-                Console.WriteLine("Jogo de Adivinhação");
-                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                ExibirMenu();
 
-                // Escolha de dificuldade
-                Console.WriteLine("Nível de dificuldade:");
-                Console.WriteLine("---------------------------------------------------------------------------------------------");
-                Console.WriteLine("1 - Fácil (10 tentativas)");
-                Console.WriteLine("2 - Normal (5 tentativas)");
-                Console.WriteLine("3 - Difícil (3 tentativas)");
-                Console.WriteLine("---------------------------------------------------------------------------------------------");
-
-                int totalDeTentativas = 0;
-
-                Console.Write("Digite sua escolha: ");
-                string escolhaDeDificuldade = Console.ReadLine();
-
-                if (escolhaDeDificuldade == "1")
-                    totalDeTentativas = 10;
-                else if (escolhaDeDificuldade == "2")
-                    totalDeTentativas = 5;
-                else
-                    totalDeTentativas = 3;
+                int totalDeTentativas = ObterNumeroDeTentativas();
 
                 // Lógica do jogo
                 Random geradorDeNumeros = new Random();
@@ -42,62 +22,114 @@ namespace JogoDeAdivinhacaoConsoleApp1
 
                 for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
                 {
-                    Console.Clear();
-                    Console.WriteLine("---------------------------------------------------------------------------------------------");
-                    Console.WriteLine($"Tentativas {tentativa} de {totalDeTentativas}");
-                    Console.WriteLine("---------------------------------------------------------------------------------------------");
+                    bool acertou = JogarTentativa(tentativa, totalDeTentativas, numerosDigitados, numeroSecreto, ref pontos);
 
-                    Console.Write("Digite um número (de 1 a 20) para chutar: ");
-                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-
-                    // Verificar se o número já foi digitado
-                    if (Array.Exists(numerosDigitados, n => n == numeroDigitado.ToString()))
+                    if (acertou)
                     {
-                        Console.WriteLine("---------------------------------------------------------------------------------------------");
-                        Console.WriteLine($"Você já chutou o número {numeroDigitado}. Tente outro.");
-                        tentativa--;
-                    }
-
-                    numerosDigitados[tentativa - 1] = numeroDigitado.ToString();
-
-                    
-                    if (numeroDigitado == numeroSecreto)
-                    {
-                        Console.WriteLine("---------------------------------------------------------------------------------------------");
-                        Console.WriteLine("Parabéns, você acertou!");
-                        Console.WriteLine("---------------------------------------------------------------------------------------------");
                         break;
                     }
-                    else
-                    {
-                        int penalidade = Math.Abs((numeroDigitado - numeroSecreto) / 2);
-                        pontos -= penalidade;
-                        Console.WriteLine("---------------------------------------------------------------------------------------------");
-                        Console.WriteLine($"Você errou! A diferença entre seu chute e o número secreto foi de {penalidade} pontos.");
-                        Console.WriteLine($"Pontuação atual: {pontos} pontos.");
-                        Console.WriteLine("---------------------------------------------------------------------------------------------");
-
-                        if (numeroDigitado > numeroSecreto)
-                        {
-                            Console.WriteLine("O número chutado foi maior que o número secreto!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("O número chutado foi menor que o número secreto!");
-                        }
-                    }
-
-                    Console.WriteLine("Pressione ENTER para continuar!");
-                    Console.ReadLine();
                 }
-
-                Console.WriteLine($"Sua pontuação final é: {pontos} pontos.");
-                Console.Write("Deseja continuar? (S/N): ");
-                string opcaoContinuar = Console.ReadLine().ToUpper();
-
-                if (opcaoContinuar != "S")
-                    break;
+                Ganhador(pontos);
+                
             }
+        }
+
+        static void ExibirMenu()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+            // Escolha de dificuldade
+            Console.WriteLine("Nível de dificuldade:");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            Console.WriteLine("1 - Fácil (10 tentativas)");
+            Console.WriteLine("2 - Normal (5 tentativas)");
+            Console.WriteLine("3 - Difícil (3 tentativas)");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+        }
+
+        static int ObterNumeroDeTentativas()
+        {
+            int totalDeTentativas = 0;
+
+            Console.Write("Digite sua escolha: ");
+            string escolhaDeDificuldade = Console.ReadLine();
+
+            if (escolhaDeDificuldade == "1")
+                totalDeTentativas = 10;
+            else if (escolhaDeDificuldade == "2")
+                totalDeTentativas = 5;
+            else
+                totalDeTentativas = 3;
+
+            return totalDeTentativas;
+        }
+
+        static bool JogarTentativa(int tentativa, int totalDeTentativas, string[] numerosDigitados, int numeroSecreto, ref int pontos)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            Console.WriteLine($"Tentativas {tentativa} de {totalDeTentativas}");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+
+            Console.Write("Digite um número (de 1 a 20) para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            // Verificar se o número já foi digitado
+            if (Array.Exists(numerosDigitados, n => n == numeroDigitado.ToString()))
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                Console.WriteLine($"Você já chutou o número {numeroDigitado}. Tente outro.");
+                tentativa--;
+            }
+
+            numerosDigitados[tentativa - 1] = numeroDigitado.ToString();
+
+
+            if (numeroDigitado == numeroSecreto)
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                Console.WriteLine("Parabéns, você acertou!");
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                return true;
+            }
+            else
+            {
+                int penalidade = Math.Abs((numeroDigitado - numeroSecreto) / 2);
+                pontos -= penalidade;
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                Console.WriteLine($"Você errou! A diferença entre seu chute e o número secreto foi de {penalidade} pontos.");
+                Console.WriteLine($"Pontuação atual: {pontos} pontos.");
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+                if (numeroDigitado > numeroSecreto)
+                {
+                    Console.WriteLine("O número chutado foi maior que o número secreto!");
+                }
+                else
+                {
+                    Console.WriteLine("O número chutado foi menor que o número secreto!");
+                }
+            }
+
+            Console.WriteLine("Pressione ENTER para continuar!");
+            Console.ReadLine();
+            return false;
+        }
+
+        static bool Ganhador(int pontos)
+        {
+            Console.WriteLine($"Sua pontuação final é: {pontos} pontos.");
+            Console.Write("Deseja continuar? (S/N): ");
+            string opcaoContinuar = Console.ReadLine().ToUpper();
+
+            if (opcaoContinuar != "S")
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
